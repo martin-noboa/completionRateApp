7743f7e5d37e0251052874bbf454ff0d23b90ca8
martin-noboaa <martin.noboa@nibbio.co>
2024-09-02 15:35:55 -0600

    Update data functions

--- dataTemplate.py
+++ dataTemplate.py
@@ -1,169 +1,45 @@
-'''Import req libraries'''
+from datetime import timedelta
 import pandas as pd
-from abc import ABC, abstractmethod
-CONTEXT_DIRECTORY = "./context/"
 
-class dataTemplate(ABC):
-    def template(self,directory,contextDirectoy):
-        self.directory = directory
-        self.contextDirectory = contextDirectoy
-        self.data = {}
-        self.data["rawData"] = self.loadData()
-        self.data["cleanData"] = self.cleanData()
-        self.data["totalLoansCount"] = self.getLoanCount()
-        self.data["completedLoansCount"] = self.getCompletedLoans()
-        self.data["businessExceptionsCount"] = self.getBusinessExceptions()
-        self.data["exceptionsCount"] = self.getExceptions()
-        self.data["completionRate"] = self.completionRate()
-        self.data["context"] = self.getContext()
-        self.data["processName"] = self.getProcessName()
-        self.data["summary"] = self.toString()
+def openFile(path):
+    data = pd.read_csv(path)
+    return data
 
-        return self.data
+def getStatusCount(data,status):
+    count, _ = data[data["Status"] == status].shape
+    return str(count)
 
-    def completionRate(self) -> float:
-        if self.data["totalLoansCount"] == 0:
-            return 0
-        totalLoans = self.data["completedLoansCount"] + self.data["businessExceptionsCount"]
-        return float("{:.2f}".format((totalLoans/ self.data["totalLoansCount"])*100))
+def getWorktime(data):
+    minutes = int(data["Worktime"]/60)
+    seconds = (data["Worktime"]%60)
+    timespan = timedelta(minutes=minutes,seconds=seconds)
+    data["Worktime"] = timespan
+    return data
 
-    def getLoanCount(self) -> int:
-        return len(self.data["cleanData"])
+def toString(data):
+    print("Complete: " + getStatusCount(data,"Complete"))
+    print("Business Exception: " + getStatusCount(data,"Business Exception"))
+    print("Locked: " + getStatusCount(data,"Locked"))
+    print("Pending " + getStatusCount(data,"Pending"))
+    print("Exception " + getStatusCount(data,"Exception"))
 
-    def cleanData(self) -> pd.DataFrame:
-        drop = ['Resource', 'Worktime', 'ExceptionReason', 'Deferred']
-        data = self.data["rawData"].drop(columns=drop)
-        data['Loaded'] = pd.to_datetime(data['Loaded'])
-        data['LastUpdated'] = pd.to_datetime(data['LastUpdated'])
-        return data.groupby('Id')
-    
-    def getBusinessExceptions(self) -> int:
-        '''Get count of business exceptions'''
-        exceptions = self.data["cleanData"].apply(lambda group: group['Tags'].str.contains('Business Exception').sum())
-        return exceptions.sum()
-
-    def getExceptions(self) -> int:
-        return self.data["totalLoansCount"] - (self.data["completedLoansCount"] + self.data["businessExceptionsCount"])
-    
-    def toString(self)->str:
-        return ("Total Loans: " + str(self.data['totalLoansCount']) +
-                "\nCompleted Loans: " + str(self.data['completedLoansCount']) +
-                "\nBusiness Exceptions: " + str(self.data['businessExceptionsCount']) +
-                "\nExceptions: " + str(self.data['exceptionsCount']) +
-                "\nCompletion Rate: " + str(self.data['completionRate']) +"%")
-
-    @abstractmethod
-    def loadData(self) -> pd.DataFrame:
-        # Read CSV file and store raw data
+def setStatus(data, completedKeyword):
+    if pd.notnull(data["Tags"]) and "Business Exception" in data["Tags"]:
+        data["Status"] = "Business Exception"
+    elif data["isLocked"] == 1:
+        data["Status"] = "Locked"
+    elif data["Status"] == completedKeyword:
+        data["Status"] = "Complete"
+    elif data["Status"] == "Pending":
         pass
+    else:
+        data["Status"] = "Exception"
+    return data
 
-    @abstractmethod
-    def getCompletedLoans(self) -> int:
-        #  Get completed loans and count number of instances
-        pass
-
-    @abstractmethod
-    def getContext(self) -> str:
-        #  Get process context
-        pass
-
-    @abstractmethod
-    def getProcessName(self) -> str:
-        #  Get process context
-        pass
-    
-class ICD(dataTemplate):
-    def loadData(self)-> pd.DataFrame:
-        filename = '/icd.csv'
-        data = pd.read_csv(self.directory+filename)
-        return data
-
-    def getCompletedLoans(self)-> int:
-        return self.data["cleanData"]['Status'].apply(lambda status: status == 'Complete').sum()
-    
-    def getContext(self) -> str:
-        contextFile = self.contextDirectory + "/icd.txt"
-        with open(contextFile, "r",encoding="utf-8") as file:
-            context = file.read()
-        return context
-    
-    def getProcessName(self) -> str:
-        return "Initial and Final Closing Disclosures"
-
-class UCD(dataTemplate):
-    def loadData(self)-> pd.DataFrame:
-        filename = '/ucd.csv'
-        data = pd.read_csv(self.directory+filename)
-        return data
-
-    def getCompletedLoans(self)-> int:
-        return self.data["cleanData"]['Status'].apply(lambda status: status == 'Completed').sum()
-    
-    def getContext(self) -> str:
-        contextFile = self.contextDirectory + "/ucd.txt"
-        with open(contextFile, "r",encoding="utf-8") as file:
-            context = file.read()
-        return context
-    
-    def getProcessName(self) -> str:
-        return "UCD/LCLA"
-    
-
-class Closing(dataTemplate):
-    def loadData(self)-> pd.DataFrame:
-        filename = '/clscmt.csv'
-        data = pd.read_csv(self.directory+filename)
-        closing = data[data['Tags'].str.contains('Closing')]
-        return closing
-
-    def getCompletedLoans(self)-> int:
-        return self.data["cleanData"]['Status'].apply(lambda status: status == 'Completed').sum()
-    
-    def getContext(self) -> str:
-        contextFile = self.contextDirectory + "/closing.txt"
-        with open(contextFile, "r",encoding="utf-8") as file:
-            context = file.read()
-        return context
-    
-    def getProcessName(self) -> str:
-        return "Closing Docs"
-    
-class Commitment(dataTemplate):
-    def loadData(self)-> pd.DataFrame:
-        filename = '/clscmt.csv'
-        data = pd.read_csv(self.directory+filename)
-        commitment = data[data['Tags'].str.contains('Commitment')]
-        return commitment
-
-    def getCompletedLoans(self)-> int:
-        return self.data["cleanData"]['Status'].apply(lambda status: status == 'Complete').sum()
-    
-    def getContext(self) -> str:
-        contextFile = self.contextDirectory + "/commitment.txt"
-        with open(contextFile, "r",encoding="utf-8") as file:
-            context = file.read()
-        return context
-    
-    def getProcessName(self) -> str:
-        return "Commitment Letter"
-
-    
-class EFolder(dataTemplate):
-    def loadData(self)-> pd.DataFrame:
-        filename = '/efolder.csv'
-        data = pd.read_csv(self.directory+filename)
-        return data
-
-    def getCompletedLoans(self)-> int:
-        return self.data["cleanData"]['Status'].apply(lambda status: status == 'Complete').sum()
-    
-    def getContext(self) -> str:
-        contextFile = self.contextDirectory + "/efolder.txt"
-        with open(contextFile, "r",encoding="utf-8") as file:
-            context = file.read()
-        return context
-    
-    def getProcessName(self) -> str:
-        return "eFolder"
-
-
+def cleanUp (data, process, completedKeyword, dropColumns=['Resource', 'Loaded','LastUpdated', 'Exception', 'Deferred','Completed','KeyValue','Priority','Attempt','Locked','ExceptionReason','isLocked']):
+    data['isLocked'] = data['Locked'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    data['Status'] = data['Status'].apply(lambda x: x if pd.notnull(x) else "Pending")
+    data = data.apply(lambda row: setStatus(row, completedKeyword), axis=1)
+    data = data.drop(columns=dropColumns)
+    data["Process"] = process
+    return data
